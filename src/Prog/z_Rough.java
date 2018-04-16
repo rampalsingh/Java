1a05e1e6c82670d4c1ce1d4a7b9cbdfe07d9991f
rampal singh <rampal.a.singh@gmail.com>
2018-04-16 14:55:54 +0530

    16/04/2017

---
+++ src/Prog/z_Rough.java
@@ -0,0 +1,11 @@
+package Prog;
+
+public class z_Rough {
+
+	public static void main(String[] args) {
+		String data = "TODO 	Auto-generated			 method stub";
+		System.out.println(data.replaceAll("\\s", ""));
+
+	}
+
+}
