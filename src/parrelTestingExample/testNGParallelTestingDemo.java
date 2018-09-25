da63ca733c0ce48f9bd441003dbc232ab1e3d4d0
MyPC <rampal.a.singh@gmail.com>
2018-09-26 02:25:39 +0530

    added testNg1.xml parallel testing

--- src/parrelTestingExample/testNGParallelTestingDemo.java
+++ src/parrelTestingExample/testNGParallelTestingDemo.java
@@ -1,21 +1,43 @@
 package parrelTestingExample;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class testNGParallelTestingDemo {
-	
+	WebDriver driver = null;
+	public static String sysPath = System.getProperty("user.dir");
+	public static String chrome = sysPath + "\\drivers\\chromedriver.exe";
+	public static String firefox = sysPath + "\\drivers\\geckodriver.exe";
+
 	@Test
-	public void test1() {
-		System.out.println("Inside Test_1 Block");
+	public void test1() throws Exception {
+		System.out.println("Inside Test_2 Block || " + Thread.currentThread().getId());
+		System.setProperty("webdriver.chrome.driver", chrome);
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.get("http://google.com");
+		Thread.sleep(2000);
+		driver.quit();
+
 	}
-	
+
 	@Test
-	public void test2() {
-		System.out.println("Inside Test_2 Block");
+	public void test2() throws Exception {
+		System.out.println("Inside Test_1 Block || " + Thread.currentThread().getId());
+		System.setProperty("webdriver.chrome.driver", chrome);
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.get("http://google.com");
+		Thread.sleep(2000);
+		driver.quit();
 	}
-	
+
 	@Test
-	public void test3() {
-		System.out.println("Inside Test_3 Block");
+	public void test3() throws Exception {
+		System.out.println("Inside Test_3 Block || " + Thread.currentThread().getId());
 	}
 }
