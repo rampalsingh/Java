28d9290622ebcb326d1ea96366667f5b284ff0b3
MyPC <rampal.a.singh@gmail.com>
2018-09-27 00:54:59 +0530

    renamed

--- src/testNgGrouping/testNgGroupingDemo.java
+++ src/testNgGrouping/testNgGroupingDemo.java
@@ -1,9 +1,6 @@
 package testNgGrouping;
 
-import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class testNgGroupingDemo {
