6c4d094c0cc91073ca508796f41d8c54f9c2a2f8
rampal.singh <rampal.singh@STSPC93.sts.local>
2018-09-26 16:45:17 +0530

    added

--- src/parrelTestingExample/testNGParallelTestingDemo.java
+++ src/parrelTestingExample/testNGParallelTestingDemo.java
@@ -8,36 +8,40 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class testNGParallelTestingDemo {
-	WebDriver driver = null;
+	private static WebDriver driver = null;
 	public static String sysPath = System.getProperty("user.dir");
 	public static String chrome = sysPath + "\\drivers\\chromedriver.exe";
 	public static String firefox = sysPath + "\\drivers\\geckodriver.exe";
 
 	@Test
 	public void test1() throws Exception {
-		System.out.println("Inside Test_2 Block || " + Thread.currentThread().getId());
-		System.setProperty("webdriver.chrome.driver", chrome);
-		driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-		driver.get("http://google.com");
-		Thread.sleep(2000);
-		driver.quit();
-
-	}
-
-	@Test
-	public void test2() throws Exception {
 		System.out.println("Inside Test_1 Block || " + Thread.currentThread().getId());
 		System.setProperty("webdriver.chrome.driver", chrome);
 		driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		driver.get("http://google.com");
 		Thread.sleep(2000);
+		System.out.println("Inside Test_1 Block ::: ENDED || " + Thread.currentThread().getId());
+		driver.quit();
+
+	}
+
+	@Test
+	public void test2() throws Exception {
+		System.out.println("Inside Test_2 Block || " + Thread.currentThread().getId());
+		System.setProperty("webdriver.chrome.driver", chrome);
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.get("http://google.com");
+		Thread.sleep(2000);
+		System.out.println("Inside Test_2 Block ::: ENDED || " + Thread.currentThread().getId());
 		driver.quit();
 	}
 
 	@Test
 	public void test3() throws Exception {
 		System.out.println("Inside Test_3 Block -> ||  " + Thread.currentThread().getId());
+		System.out.println("Inside Test_3 Block ::: ENDED || " + Thread.currentThread().getId());
+		driver.quit();
 	}
 }
