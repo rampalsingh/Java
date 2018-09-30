40f3a4ad008d6751484336a2bf894119f7bcdf2b
rampal <rampal.a.singh@gmail.com>
2018-10-01 01:10:29 +0530

    conflicts resolved

--- src/testNgIgnore/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * @author rampal.singh
- *
- */
-@org.testng.annotations.Ignore
--- src/testNgIgnore/testNgIgnoreDemo.java
+++ src/testNgIgnore/testNgIgnoreDemo.java
@@ -15,21 +15,3 @@ public class testNgIgnoreDemo  {
 		System.out.println("Inside Test_2 Block");
 	}
 }
-=======
-package testNgIgnore;
-
-import org.testng.annotations.Test;
-
-//@Ignore
-public class testNgIgnoreDemo  {
-
-	@Test
-	public void test1() {
-		System.out.println("Inside Test_1 Block");
-	}
-
-	@Test
-	public void test2() {
-		System.out.println("Inside Test_2 Block");
-	}
-}
